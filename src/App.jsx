7fa014004a5372daa2672a77c6224f3b4852e388
brockstar299 <150369458+brockstar299@users.noreply.github.com>
2025-02-05 01:55:51 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="Millie.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Millie
         </p>
       </header>
     </div>
